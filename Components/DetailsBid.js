6ac7a9c62919562a4634edcbfb94eff13f9cb555
Vlad20086 <63899865+Vlad20086@users.noreply.github.com>
2023-05-14 14:11:44 +0300

    nft-ui app

---
+++ Components/DetailsBid.js
@@ -0,0 +1,43 @@
+import { View, Text,Image } from 'react-native'
+import React from 'react'
+import { EthPrice } from './SubInfo';
+import { COLORS, SIZES, FONTS } from "../constants";
+
+const DetailsBid = ({bid}) => {
+  return (
+    <View style={{
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginVertical: SIZES.base,
+      paddingHorizontal: SIZES.base * 2,
+    }}>
+      <Image 
+        source={bid.image}
+        resizeMode='contain'
+        style={{width: 48, height: 48}}
+      />
+      <View>
+        <Text style={{
+          fontFamily:FONTS.semiBold,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+          }}>
+          Comanda plasata de {bid.name}
+        </Text>
+        <Text style={{
+          fontFamily:FONTS.semiBold,
+          fontSize: SIZES.small - 2,
+          color: COLORS.primary,
+          marginTop: 3,
+          }}>
+          Comanda plasata pe {bid.date}
+        </Text>
+      </View>
+      <EthPrice price={bid.price} />
+    </View>
+  )
+}
+
+export default DetailsBid
